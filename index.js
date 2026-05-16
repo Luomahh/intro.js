224bafaeaf1f7673d40f2d85761d5d6ca5c7de3a
Luomahh <ascolojiakuprecious@gmail.com>
2026-05-16 18:25:02 +0100

    add intro.js code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('hello, world!');
+const name = "Precious";
+const surname = "Ojiaku"
+const age = 22;
+const favoriteLanguage = "Node.js";
+
+console.log("Name:", name);
+console.log("Surname:", surname);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
---
+++ intro.js
@@ -0,0 +1,9 @@
+const name = "Precious";
+const surname = "Ojiaku"
+const age = 22;
+const favoriteLanguage = "Node.js";
+
+console.log("Name:", name);
+console.log("Surname:", surname);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
