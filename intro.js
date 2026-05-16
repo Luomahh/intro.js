6846bf31a441dc8b88768e4312aa9f45c0fd0df4
Luomahh <ascolojiakuprecious@gmail.com>
2026-05-16 18:16:33 +0100

    Betechified week one assignment

--- intro.js
+++
@@ -1,7 +0,0 @@
-const name = "Precious";
-const age = 22;
-const favoriteLanguage = "Node.js";
-
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("Favorite Programming Language:", favoriteLanguage);
